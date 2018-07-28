ade4f70753f6785d1a9c3a872a1793fc437bf483
Bob Schellink <sabob1@gmail.com>
2018-07-28 22:41:47 +0200

    added functionalinterface

--- src/main/java/za/sabob/tempo/transaction/TransactionalOperation.java
+++ src/main/java/za/sabob/tempo/transaction/TransactionalOperation.java
@@ -2,6 +2,7 @@ package za.sabob.tempo.transaction;
 
 import javax.persistence.*;
 
+@FunctionalInterface
 public interface TransactionalOperation<X extends Exception> {
 
     public void run( EntityManager em ) throws X;
--- src/main/java/za/sabob/tempo/transaction/TransactionalQuery.java
+++ src/main/java/za/sabob/tempo/transaction/TransactionalQuery.java
@@ -2,6 +2,7 @@ package za.sabob.tempo.transaction;
 
 import javax.persistence.*;
 
+@FunctionalInterface
 public interface TransactionalQuery<T, X extends Exception> {
 
     public T get( EntityManager em ) throws X;
